36cb06b40c4063248dadc8c4db5d61dccae041ed
James Martin <jamesmartin@github.com>
2020-03-20 13:16:59 +1100

    Basic function to get template contents

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+async function getTemplate(token, owner, repo, path) {
+  const octokit = github.GitHub(token)
+  const { data: contents } = await octokit.repos.getContents({
+    owner,
+    repo,
+    path
+  })
+
+  return contents
+}
+
 try {
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
   const template = core.getInput('template')
   const projectColumnId = core.getInput('project-column-id')
   const personalAccessToken = core.getInput('personal-access-token')
   const githubToken = core.getInput('github-token')
+
+  // 1. Get template
+  const templateContents = await getTemplate(githubToken, owner, repo, template)
+  console.log(templateContents)
+
+  // 2. Get project card content URLs and descriptions
+  // 3. Open a PR based on the template and the cards
 
   console.log('Template: ' + template)
   console.log('Project Column ID: ' + projectColumnId)
