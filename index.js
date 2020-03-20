263027f9e1dd05322078ed08031389fc6fea604a
James Martin <jamesmartin@github.com>
2020-03-20 12:33:46 +1100

    Add required GITHUB_TOKEN input

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ try {
   const template = core.getInput('template')
   const projectColumnId = core.getInput('project-column-id')
   const personalAccessToken = core.getInput('personal-access-token')
+  const githubToken = core.getInput('github-token')
 
   console.log('Template: ' + template)
   console.log('Project Column ID: ' + projectColumnId)
   console.log('Personal Access Token (last 8): ' + personalAccessToken.slice(personalAccessToken.length - 8))
+  console.log('GitHub Token (last 8): ' + githubToken.slice(githubToken.length - 8))
 } catch (error) {
   core.setFailed(error.message);
 }
