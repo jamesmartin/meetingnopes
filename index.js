f35e0bf5dd0e420223b3bb857780b6dcd3f54c82
James Martin <jamesmartin@github.com>
2020-03-21 06:52:24 +1100

    Use Mustache to render the notes file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const things = require('./apiThings.js')
 const core = require('@actions/core');
 const github = require('@actions/github');
+const Mustache = require('mustache')
 
 try {
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
@@ -9,13 +10,24 @@ try {
   const personalAccessToken = core.getInput('personal-access-token')
   const githubToken = core.getInput('github-token')
   const octokit = new github.GitHub(githubToken)
+  const octokitPat = new github.GitHub(personalAccessToken)
 
   // 1. Get template
-  gt.getTemplate(octokit, owner, repo, template).then((data) => {
-    console.log(data)
+  things.getTemplate(octokit, owner, repo, template).then((template) => {
+
+    // 2. Get project card content URLs and descriptions
+    things.getProjectCards(octokitPat, projectColumnId).then(cards => {
+
+      // 2.1 Build the initial notes document using the template
+      const view = {
+        date: (new Date()).toDateString(),
+        cards: cards
+      }
+      const rendered = Mustache.render(template, view)
+      console.log(rendered)
+    })
   })
 
-  // 2. Get project card content URLs and descriptions
   // 3. Open a PR based on the template and the cards
 
   console.log('Template: ' + template)
