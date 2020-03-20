b2ce3ca823515e89e67c66eb1bf8bea7703c75dc
James Martin <jamesmartin@github.com>
2020-03-20 11:41:46 +1100

    Allow a PAT to be passed to access Projects

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const github = require('@actions/github');
 try {
   const template = core.getInput('template')
   const projectColumnId = core.getInput('project-column-id')
+  const personalAccessToken = core.getInput('personal-access-token')
 
   console.log('Template: ' + template)
   console.log('Project Column ID: ' + projectColumnId)
+  console.log('Personal Access Token (last 5): ' + personalAccessToken.slice(personalAccessToken.length - 5))
 } catch (error) {
   core.setFailed(error.message);
 }
